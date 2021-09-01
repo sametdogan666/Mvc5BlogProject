42c227d16ba0dfb04b89f97c6fd861c3d7cd5662
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-01 17:53:54 +0300

    Context Sinifi Olusturuldu Ve Class'in Icerisi Dolduruldu.

---
+++ DataAccess/Concrete/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
