722fffaa7b656989c7f0bf06c263113124d57413
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-02 21:21:35 +0300

    Repository Class'i Yazildi

---
+++ DataAccess/Concrete/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Abstract;
+
+namespace DataAccess.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        BlogContext blogContext = new BlogContext();
+        private DbSet<T> _object;
+
+        public Repository()
+        {
+            _object = blogContext.Set<T>();
+        }
+        public int Delete(T p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(T p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(T p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
