624302eab709f0284c668423e5b6ae54f089ccf1
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-02 19:19:52 +0300

    Repository Design Pattern'a Giris Yapildi

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        int Insert(T p);
+        int Update(T p);
+        int Delete(T p);
+        T GetById(int id);
+    }
+}
