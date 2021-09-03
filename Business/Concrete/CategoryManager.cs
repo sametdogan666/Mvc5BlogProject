a0f471ca1641c03cfb716b336248ecb3d5bc0c99
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-03 21:12:39 +0300

    Kategorilerin Listelenmesi Yapildi

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Concrete;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CategoryManager
+    {
+        Repository<Category> repoCategory = new Repository<Category>();
+        public List<Category> GetAll()
+        {
+            return repoCategory.List();
+        }
+    }
+}
---
+++ Mvc5BlogProject/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business.Concrete;
+
+namespace Mvc5BlogProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager categoryManager = new CategoryManager();
+        public ActionResult Index()
+        {
+            var categoryValues = categoryManager.GetAll();
+            return View(categoryValues);
+        }
+    }
+}
