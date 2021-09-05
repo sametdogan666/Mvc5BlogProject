e317e629565dbfa45aa30529cde69345d21c95fb
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-05 14:46:54 +0300

    Author About Partial Ve Author Popular Post Partial Eklendi

---
+++ Mvc5BlogProject/Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5BlogProject.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        public PartialViewResult AuthorAbout()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AuthorPopularPost()
+        {
+            return PartialView();
+        }
+    }
+}
