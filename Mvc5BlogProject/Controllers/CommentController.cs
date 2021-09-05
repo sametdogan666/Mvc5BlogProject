92b5a99e0566ca4d71cc70d61223112e36c9c503
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-05 14:34:38 +0300

    Comment List Partial Ve Leave Comment Partial Eklendi

---
+++ Mvc5BlogProject/Controllers/CommentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5BlogProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        // GET: Comment
+        public PartialViewResult CommentList()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult LeaveComment()
+        {
+            return PartialView();
+        }
+    }
+}
