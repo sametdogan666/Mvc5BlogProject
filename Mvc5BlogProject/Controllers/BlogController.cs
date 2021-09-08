253a64dcfb82786ac69df9a107f8ae1a5ab319d0
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-08 13:52:41 +0300

    One Cikan Postlarda 4 Post Tamamlandi. Belirlenen Kategorilerin Son Postlari One Cikariliyor

--- Mvc5BlogProject/Controllers/BlogController.cs
+++ Mvc5BlogProject/Controllers/BlogController.cs
@@ -24,6 +24,50 @@ namespace Mvc5BlogProject.Controllers
 
         public PartialViewResult FeaturedPosts()
         {
+            //1.Post
+            var postTitle1 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 4).Select(y => y.BlogTitle)
+                .FirstOrDefault();
+            var postImage1 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 4).Select(y => y.BlogImage)
+                .FirstOrDefault();
+            var blogDate1 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 4).Select(y => y.BlogDate)
+                .FirstOrDefault();
+            ViewBag.postTitle1 = postTitle1;
+            ViewBag.postImage1 = postImage1;
+            ViewBag.blogDate1 = blogDate1;
+
+            //2.Post
+            var postTitle2 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 1).Select(y => y.BlogTitle)
+                .FirstOrDefault();
+            var postImage2 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 1).Select(y => y.BlogImage)
+                .FirstOrDefault();
+            var blogDate2 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 1).Select(y => y.BlogDate)
+                .FirstOrDefault();
+            ViewBag.postTitle2 = postTitle2;
+            ViewBag.postImage2 = postImage2;
+            ViewBag.blogDate2 = blogDate2;
+
+            //3.Post
+            var postTitle3 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 3).Select(y => y.BlogTitle)
+                .FirstOrDefault();
+            var postImage3 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 3).Select(y => y.BlogImage)
+                .FirstOrDefault();
+            var blogDate3 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 3).Select(y => y.BlogDate)
+                .FirstOrDefault();
+            ViewBag.postTitle3 = postTitle3;
+            ViewBag.postImage3 = postImage3;
+            ViewBag.blogDate3 = blogDate3;
+
+            //4.Post
+            var postTitle4 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 6).Select(y => y.BlogTitle)
+                .FirstOrDefault();
+            var postImage4 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 6).Select(y => y.BlogImage)
+                .FirstOrDefault();
+            var blogDate4 = _blogManager.GetAll().OrderByDescending(z => z.BlogId).Where(x => x.CategoryId == 6).Select(y => y.BlogDate)
+                .FirstOrDefault();
+            ViewBag.postTitle4 = postTitle4;
+            ViewBag.postImage4 = postImage4;
+            ViewBag.blogDate4 = blogDate4;
+
             return PartialView();
         }
 
