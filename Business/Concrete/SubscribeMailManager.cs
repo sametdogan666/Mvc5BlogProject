a88d12b4fdaf7d56ba4e178e739634f68e27463b
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-17 15:37:30 +0300

    Mail Subscribe Business Katmani Yazildi

---
+++ Business/Concrete/SubscribeMailManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Concrete;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class SubscribeMailManager
+    {
+        Repository<SubscribeMail> repositorySubscribeMail =new Repository<SubscribeMail>();
+        public int Add(SubscribeMail subscribeMail)
+        {
+            if (subscribeMail.Mail.Length<=10 && subscribeMail.Mail.Length>=50)
+            {
+                return -1;
+            }
+
+            return repositorySubscribeMail.Insert(subscribeMail);
+        }
+    }
+}
