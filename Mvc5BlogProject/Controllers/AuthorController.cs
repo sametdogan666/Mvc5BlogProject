bd87f5c04ad5505503f05b91b4e0b0540d18e8af
Samet DOGAN <s.doqann1570@gmail.com>
2021-09-22 09:29:04 +0300

    Author About Tamamlandi.

--- Mvc5BlogProject/Controllers/AuthorController.cs
+++ Mvc5BlogProject/Controllers/AuthorController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Business.Concrete;
 
 namespace Mvc5BlogProject.Controllers
 {
     public class AuthorController : Controller
     {
         // GET: Author
-        public PartialViewResult AuthorAbout()
+        BlogManager _blogManager = new BlogManager();
+        public PartialViewResult AuthorAbout(int id)
         {
-            return PartialView();
+            var authorDetail = _blogManager.GetBlogById(id);
+            return PartialView(authorDetail);
         }
 
         public PartialViewResult AuthorPopularPost()
