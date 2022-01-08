37c7c68f4410fcb0a86dbac10984a14fb82b30c7
Samet DOGAN <s.doqann1570@gmail.com>
2022-01-08 14:57:37 +0300

    Yazar Login Formu Oluşturuldu

---
+++ Mvc5BlogProject/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5BlogProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult AuthorLogin()
+        {
+            return View();
+        }
+    }
+}
