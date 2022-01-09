452e9af7ef1b9cc209be337d7d646fefac3937bd
Samet DOGAN <s.doqann1570@gmail.com>
2022-01-09 19:40:00 +0300

    Yeni Blog Ekleme Islemi Tamamlandi

--- Mvc5BlogProject/Controllers/UserController.cs
+++ Mvc5BlogProject/Controllers/UserController.cs
@@ -64,5 +64,35 @@ namespace Mvc5BlogProject.Controllers
             _blogManager.UpdateBlog(blog);
             return RedirectToAction("BlogList");
         }
+
+
+        [HttpGet]
+        public ActionResult AddNewBlog()
+        {
+            BlogContext blogContext = new BlogContext();
+            List<SelectListItem> values = (from x in blogContext.Categories.ToList()
+                select new SelectListItem()
+                {
+                    Text = x.CategoryName,
+                    Value = x.CategoryId.ToString()
+                }).ToList();
+            ViewBag.values = values;
+
+            List<SelectListItem> values2 = (from x in blogContext.Authors.ToList()
+                select new SelectListItem()
+                {
+                    Text = x.AuthorName,
+                    Value = x.AuthorId.ToString()
+                }).ToList();
+            ViewBag.values2 = values2;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddNewBlog(Blog blog)
+        {
+            _blogManager.BlogAddBM(blog);
+            return RedirectToAction("BlogList");
+        }
+
     }
 }
